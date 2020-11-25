d386b120f583851d30df74bdd99b62e1ad7d6c66
Ning Li <ning.li@nag.co.uk>
2020-11-25 13:30:17 +0000

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,5 @@
 ckass profile::agent_nodes {
   include dockeragent
-  dockeragent::nodes {'web.puppet.vm':}
-  dockeragent::nodes {'db.puppet.vm':}
+  dockeragent::node {'web.puppet.vm':}
+  dockeragent::node {'db.puppet.vm':}
 }
